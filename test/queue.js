f5e191c241b1259ae8d4c9159d93f6034e0ff263
jmjuanes <josemijuanes@gmail.com>
2018-03-16 16:06:33 +0100

    Added new tests to queue

--- test/queue.js
+++ test/queue.js
@@ -14,7 +14,7 @@ describe("queue", function () {
             return next();
         });
         q.then(function (next) {
-            return kofi.delay(10, function () {
+            return kofi.delay(100, function () {
                 e3 = true;
                 return next();
             });
@@ -26,6 +26,32 @@ describe("queue", function () {
             assert.equal(e1, true);
             assert.equal(e2, true);
             assert.equal(e3, true);
+            return done();
+        });
+    });
+    it("calls the provided function with catch if there is an error in a task", function (done) {
+        let q = kofi.queue();
+        let e1 = false, e2 = false, e3 = false;
+        q.then(function (next) {
+            e1 = true;
+            return next();
+        });
+        q.then(function (next) {
+            e2 = true;
+            return next(new Error(""));
+        });
+        q.then(function (next) {
+            e3 = true;
+            return next();
+        });
+        q.finish(function () {
+            return done(new Error(""));
+        });
+        q.catch(function (error) {
+            assert.equal(typeof error, "object");
+            assert.equal(e1, true);
+            assert.equal(e2, true);
+            assert.equal(e3, false);
             return done();
         });
     });
