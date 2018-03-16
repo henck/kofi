259f3059ffa6f3091dc95e5331c51ea5a9c9ff59
jmjuanes <josemijuanes@gmail.com>
2018-03-16 14:18:00 +0100

    Init concat tests

---
+++ test/arrays.js
@@ -0,0 +1,31 @@
+let assert = require("assert");
+let kofi = require("../index.js");
+
+describe("concat", function () {
+    it("concatenates arrays", function (done) {
+        let arr1 = [1, 2, 3];
+        let arr2 = [4, 5, 6, 7];
+        let arr3 = [8];
+        let concat = kofi.concat(arr1, arr2, arr3);
+        assert.equal(Array.isArray(concat), true);
+        assert.equal(concat.length, 8);
+        assert.equal(concat[0], 1);
+        assert.equal(concat[7], 8);
+        assert.equal(concat[5], 6);
+        return done();
+    });
+
+    it("concatenates non-array values", function (done) {
+        let v1 = true;
+        let v2 = null;
+        let v3 = 0;
+        let v4 = [1, 2, 3];
+        let concat = kofi.concat(v1, v2, v3, v4);
+        assert.equal(Array.isArray(concat), true);
+        assert.equal(concat.length, 6);
+        assert.equal(concat[0], true);
+        assert.equal(concat[3], 1);
+        return done();
+    });
+});
+
