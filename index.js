af065bde487ce07270b5f54ddebda97ca35cee70
jmjuanes <josemijuanes@gmail.com>
2018-03-16 13:52:57 +0100

    Added queue management

--- index.js
+++ index.js
@@ -46,6 +46,58 @@ module.exports.event = function () {
     return dispatch;
 };
 
+//Queue management
+module.exports.queue = function (fn) {
+    let taskList = [];
+    let taskError = function() {
+        return null;
+    };
+    if (typeof fn === "function") {
+        taskList.push(fn);
+    }
+    let q = {
+        then: function (fn) {
+            if (typeof fn === "function") {
+                taskList.push(fn);
+            }
+            return q;
+        },
+        catch: function (fn) {
+            if (typeof fn === "function") {
+                taskError = fn;
+            }
+            return q;
+        },
+        finish: function (fn) {
+            let numTasks = taskList.length;
+            let taskRun = function (index) {
+                if (index >= numTasks) {
+                    if (typeof fn === "function") {
+                        return fn.call(null);
+                    }
+                    return;
+                }
+                //Execute the next function
+                return taskList[index].call(null, function (error) {
+                    if (error) {
+                        //Call the task error 
+                        return taskError.call(null, error);
+                    }
+                    //Continue with the next function in the list
+                    return taskRun(index + 1);
+                });
+            };
+            //Terrible hack to run this task in async mode
+            //TO_DO: change this with a method like process.nextTick in node.js
+            setTimeout(function () {
+                return taskRun(0);
+            }, 0);
+            return q;
+        }
+    };
+    return q;
+};
+
 //Set interval wrapper 
 module.exports.timer = function (time, fn) {
     return setInterval(fn, time);
