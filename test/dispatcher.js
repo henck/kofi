0ab7cc8afb96175547eddc2938e0747854d6ae2a
jmjuanes <josemijuanes@gmail.com>
2018-03-16 17:45:18 +0100

    Added dispatcher tests

---
+++ test/dispatcher.js
@@ -0,0 +1,26 @@
+let assert = require("assert");
+let kofi = require("../index.js");
+
+describe("dispatch", function () {
+    it("calls all listeners associated with the same event name", function (done) {
+        let e = kofi.dispatch();
+        let call1 = false, call2 = false;
+        e.on("my-event", function (value) {
+            call1 = true;
+            assert.equal(value, true);
+        });
+        e.on("my-event", function (value) {
+            call2 = true;
+            assert.equal(value, true);
+        });
+        e.emit("my-event", true);
+        return done();
+    });
+
+    it("does nothing if there is not listener registered", function (done) {
+        let e = kofi.dispatch();
+        e.emit("undef-event", null);
+        return done();
+    });
+});
+
