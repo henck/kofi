b813ac5bf997f032216f392103c1d01ae543aeda
jmjuanes <josemijuanes@gmail.com>
2018-03-16 23:47:27 +0100

    Minor changes

--- index.js
+++ index.js
@@ -58,46 +58,45 @@ module.exports.queue = function (fn) {
         taskList.push(fn);
     }
     //Queue management
-    let q = {
-        then: function (fn) {
-            if (typeof fn === "function") {
-                taskList.push(fn);
-            }
-            return q;
-        },
-        catch: function (fn) {
-            if (typeof fn === "function") {
-                taskError = fn;
-            }
-            return q;
-        },
-        finish: function (fn) {
-            let numTasks = taskList.length;
-            let taskRun = function (index) {
-                //Check for no more tasks to execute
-                if (index >= numTasks) {
-                    if (typeof fn === "function") {
-                        return fn.call(null);
-                    }
-                    return;
-                }
-                //Execute the next function
-                return taskList[index].call(null, function (error) {
-                    if (error) {
-                        //Call the task error 
-                        return taskError.call(null, error);
-                    }
-                    //Continue with the next function in the list
-                    return taskRun(index + 1);
-                });
-            };
-            //Terrible hack to run this task in async mode
-            //TO_DO: change this with a method like process.nextTick in node.js
-            setTimeout(function () {
-                return taskRun(0);
-            }, 0);
-            return q;
+    let q = {};
+    q.then = function (fn) {
+        if (typeof fn === "function") {
+            taskList.push(fn);
         }
+        return q;
+    };
+    q.catch = function (fn) {
+        if (typeof fn === "function") {
+            taskError = fn;
+        }
+        return q;
+    };
+    q.finish = function (fn) {
+        let numTasks = taskList.length;
+        let taskRun = function (index) {
+            //Check for no more tasks to execute
+            if (index >= numTasks) {
+                if (typeof fn === "function") {
+                    return fn.call(null);
+                }
+                return;
+            }
+            //Execute the next function
+            return taskList[index].call(null, function (error) {
+                if (error) {
+                    //Call the task error 
+                    return taskError.call(null, error);
+                }
+                //Continue with the next function in the list
+                return taskRun(index + 1);
+            });
+        };
+        //Terrible hack to run this task in async mode
+        //TO_DO: change this with a method like process.nextTick in node.js
+        setTimeout(function () {
+            return taskRun(0);
+        }, 0);
+        return q;
     };
     return q;
 };
