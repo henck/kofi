ea5c678d619f5e6becb792d9ca0b0ca2e8dbe233
jmjuanes <josemijuanes@gmail.com>
2018-03-18 00:02:34 +0100

    Added eachObj test

--- test/objects.js
+++ test/objects.js
@@ -8,6 +8,33 @@ let obj = {
     city: "Tokio"
 };
 
+describe("eachObj", function () {
+    it("executes a function for ach pair key-value", function (done) {
+        let keys = ["name", "age", "city"];
+        let values = ["Bob", 30, "Tokio"];
+        let j = 0;
+        kofi.eachObj(obj, function (key, value) {
+            assert.equal(keys[j], key);
+            assert.equal(values[j], value);
+            j = j + 1;;
+        });
+        assert.equal(j, 3);
+        return done();
+    });
+
+    it("stops when a false boolean is returned", function (done) {
+        let j = 0;
+        kofi.eachObj(obj, function () {
+            j = j + 1;
+            if (j === 2) {
+                return false;
+            }
+        });
+        assert.equal(j, 2);
+        return done();
+    });
+});
+
 describe("keys", function () {
     it("returns all keys of an object", function (done) {
         let keys = kofi.keys(obj);
