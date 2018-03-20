5ca751c4686855de263f5ce6cee2ee24df3bf5b9
jmjuanes <josemijuanes@gmail.com>
2018-03-20 23:38:27 +0100

    Init 'sign' method to get the sign of the provided number

---
+++ src/sign.js
@@ -0,0 +1,17 @@
+/**
+ * @function sign(num)
+ * @description Returns the sign of `num`.
+ * @example
+ * sign(-45);
+ * // -> -1
+ * sign(62);
+ * // -> 1
+ */
+
+export default function sign (num) {
+    if(typeof num === "number") {
+        return (num < 0) ? -1 : 1;
+    }
+    return 1:
+}
+
