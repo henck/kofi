1f8f7a3c4c127e962ecdcabeb3255b56c98d018b
jmjuanes <josemijuanes@gmail.com>
2018-03-28 18:24:34 +0200

    Added cleanup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import cleanup from "rollup-plugin-cleanup";
+
 export default {
     "input": "./index.js",
     "output": {
         "file": "./dist/kofi.js",
         "format": "umd",
         "name": "kofi"
-    }
+    },
+    "plugins": [
+        cleanup()
+    ]
 }
