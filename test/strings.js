2ec93eb5ddd1039819729c006bf689254a2f1069
jmjuanes <josemijuanes@gmail.com>
2018-03-29 11:29:10 +0200

    Added capitalize and camel-case tests

--- test/strings.js
+++ test/strings.js
@@ -1,6 +1,20 @@
 let assert = require("assert");
 let kofi = require("../dist/index.js");
 
+describe("camelCase", function () {
+    it("converts a string to camel-case format", function (done) {
+        assert.equal(kofi.camelCase("hello world"), "helloWorld");
+        return done();
+    });
+});
+
+describe("capitalize", function () {
+    it("capitalizes a string", function (done) {
+        assert.equal(kofi.capitalize("hello world"), "Hello world");
+        return done();
+    });
+});
+
 describe("uniqueId", function () {
     it("generates unique ids", function (done) {
         let ids = [];
