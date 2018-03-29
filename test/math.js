2f1799810d72a3260ec0756eac1d349f0dd77f08
jmjuanes <josemijuanes@gmail.com>
2018-03-29 10:51:34 +0200

    Added max and min tests

--- test/math.js
+++ test/math.js
@@ -1,6 +1,22 @@
 let assert = require("assert");
 let kofi = require("../dist/index.js");
 
+describe("max", function () {
+    it("returns the max value in an array", function (done) {
+        let array = [0, -10, 5, 2, 4, 0];
+        assert.equal(kofi.max(array), 5);
+        return done();
+    });
+});
+
+describe("min", function () {
+    it("returns the min value in an array", function (done) {
+        let array = [-1, 0, 4, -5, 7];
+        assert.equal(kofi.min(array), -5);
+        return done();
+    });
+});
+
 describe("range", function () {
     it("generates a correct array range", function (done) {
         let range = kofi.range(0, 5);
