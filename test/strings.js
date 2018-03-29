2f5c61e75bd604dbd7651231de3a7149dae883a0
jmjuanes <josemijuanes@gmail.com>
2018-03-29 14:05:44 +0200

    Added more tests for string utils

--- test/strings.js
+++ test/strings.js
@@ -20,6 +20,28 @@ describe("capitalize", function () {
     });
 });
 
+describe("kebabCase", function () {
+    it("converts a string to kebab-case format", function (done) {
+        assert.equal(kofi.kebabCase("hello world"), "hello-world");
+        return done();
+    });
+});
+
+describe("repeat", function () {
+    it("repeats a string a provided number of times", function (done) {
+        assert.equal(kofi.repeat("x", 5), "xxxxx");
+        assert.equal(kofi.repeat("abc", 3), "abcabcabc");
+        return done();
+    });
+});
+
+describe("snakeCase", function () {
+    it("converts a string to snake-case format", function (done) {
+        assert.equal(kofi.snakeCase("hello world"), "hello_world");
+        return done();
+    });
+});
+
 describe("uniqueId", function () {
     it("generates unique ids", function (done) {
         let ids = [];
