3a138d351cf27125973d7b4520c6be45ca37d125
jmjuanes <josemijuanes@gmail.com>
2018-03-29 11:40:33 +0200

    Filled value can be a string or a number

--- src/fill.js
+++ src/fill.js
@@ -1,6 +1,6 @@
 /**
  * @function fill(length, value)
- * @descript Returns a new array with size `length` filled with `value`.
+ * @descript Returns a new array with size `length` filled with `value`. Only `string` or `number` values are allowed. 
  * @example 
  * fill(5, 0);
  * // -> [0, 0, 0, 0, 0]
@@ -8,7 +8,15 @@
  * // -> ["abc", "abc", "abc"]
  */
 
-export default function fill (length, value) { 
-    return Array.apply(null, Array(length)).map(Number.prototype.valueOf, value);
+export default function fill (length, value) {
+    //Create an array with the provided length
+    let array = Array.apply(null, Array(length));
+    if (typeof value === "number") {
+        return array.map(Number.prototype.valueOf, value);
+    } else if (typeof value === "string") {
+        return array.map(String.prototype.valueOf, value);
+    } else {
+        return array;
+    }
 }
 
