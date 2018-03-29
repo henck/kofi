2ab0c94693aa99c41d3d29ffd0fc3790d4083bf6
jmjuanes <josemijuanes@gmail.com>
2018-03-29 17:24:49 +0200

    Added license banner. Removed cleanup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,35 @@
-import cleanup from "rollup-plugin-cleanup";
-import uglify from "rollup-plugin-uglify";
+let uglify = require("rollup-plugin-uglify");
+let pkg = require("./package.json");
 
-let config = {"input": "./index.js"};
+//Generate the banner
+let banner = [];
+banner.push("/**");
+banner.push(" * @license kofi v" + pkg.version);
+banner.push(" *");
+banner.push(" * This source code is licensed under the MIT license found in the");
+banner.push(" * LICENSE file in the root directory of this source tree.");
+banner.push(" */");
+banner.push("");
+
+//Initialize the configuration object
+let config = {
+    "input": "./index.js",
+    "output": {
+        "format": "umd",
+        "name": "kofi",
+        "banner": banner.join("\n")
+    },
+    "plugins": []
+};
+
+//Check for production build
 let isMin = typeof process.env.MINIFY === "string" && process.env.MINIFY === "true";
 
 if(isMin === true) {
     config.plugins = [uglify()];
-    config.output = {
-        "file": "./dist/kofi.min.js",
-        "format": "umd",
-        "name": "kofi"
-    };
+    config.output.file = "./dist/kofi.min.js";
 } else {
-    config.plugins = [cleanup()];
-    config.output = {
-        "file": "./dist/kofi.js",
-        "format": "umd",
-        "name": "kofi"
-    };
+    config.output.file = "./dist/kofi.js";
 }
 
 export default config;
