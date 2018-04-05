1b103bfcdf310fc87822144168f80a1f22774f75
jmjuanes <josemijuanes@gmail.com>
2018-04-05 14:09:05 +0200

    Added omit method to clone an object without the provided keys

---
+++ src/omit.js
@@ -0,0 +1,23 @@
+/**
+ * @function omit(obj, keys)
+ * @description Created an object with all the keys of `obj` that are not in `keys`.
+ * @example 
+ * omit({a: 1, b: 2, c: 3}, ["b"]) // --> {a: 1, c: 3}
+ */
+
+export default function omit (obj, keys) {
+    //Check for no object or null
+    if (typeof obj !== "object" || obj === null) {
+        return {};
+    }
+    //Clone the object 
+    let newObj = Object.assign({}, obj);
+    //Remove selected keys 
+    keys.forEach(function (key) {
+        if (typeof newObj[key] !== "undefined") {
+            delete newObj[key];
+        } 
+    });
+    return newObj;
+}
+
