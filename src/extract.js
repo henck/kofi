18a28bee3b8f8bf64b9b8f8bd69f294e2221c86b
jmjuanes <josemijuanes@gmail.com>
2018-04-05 14:21:13 +0200

    Init extract method to create a new object only wit the provided keys

---
+++ src/extract.js
@@ -0,0 +1,23 @@
+/**
+ * @function extrack(obj, keys)
+ * @description Created a new object with only the provided keys of `obj`.
+ * @since 0.1.1
+ * @example
+ * extract({a: 1, b: 2, c: 3}, ["b"]) // --> {b: 2}
+ */
+
+export default function extract (obj, keys) {
+    //Check the provided object 
+    if (typeof obj !== "object" || obj === null) {
+        return {};
+    }
+    let newObj = {};
+    //Clone only the provided keys 
+    keys.forEach(function (key) {
+        if (typeof obj[key] !== "undefined") {
+            newObj[key] = obj[key];
+        }
+    });
+    return newObj;
+}
+
