f489bf71ce3c8aedf9574928cef488d82764805f
jmjuanes <josemijuanes@gmail.com>
2018-05-03 11:34:58 +0200

    Init kofi entry file

---
+++ packages/kofi/index.js
@@ -0,0 +1,41 @@
+/**
+ * WARNING - THIS IS AN AUTO-GENERATED FILE. DO NOT EDIT IT DIRECTLY
+ */
+
+//Export modules from kofi-dispatch
+export {default as dispatch} from "../kofi-dispatch/src/dispatch.js";
+
+//Export modules from kofi-queue
+export {default as queue} from "../kofi-queue/src/queue.js";
+
+//Export modules from kofi-request
+export {default as request} from "../kofi-request/src/request.js";
+
+//Export modules from kofi-utils
+export {default as average} from "../kofi-utils/src/average.js";
+export {default as camelCase} from "../kofi-utils/src/camel-case.js";
+export {default as capitalize} from "../kofi-utils/src/capitalize.js";
+export {default as concat} from "../kofi-utils/src/concat.js";
+export {default as delay} from "../kofi-utils/src/delay.js";
+export {default as digits} from "../kofi-utils/src/digits.js";
+export {default as each} from "../kofi-utils/src/each.js";
+export {default as extract} from "../kofi-utils/src/extract.js";
+export {default as fill} from "../kofi-utils/src/fill.js";
+export {default as format} from "../kofi-utils/src/format.js";
+export {default as isEmpty} from "../kofi-utils/src/is-empty.js";
+export {default as kebabCase} from "../kofi-utils/src/kebab-case.js";
+export {default as keys} from "../kofi-utils/src/keys.js";
+export {default as max} from "../kofi-utils/src/max.js";
+export {default as min} from "../kofi-utils/src/min.js";
+export {default as omit} from "../kofi-utils/src/omit.js";
+export {default as pad} from "../kofi-utils/src/pad.js";
+export {default as random} from "../kofi-utils/src/random.js";
+export {default as range} from "../kofi-utils/src/range.js";
+export {default as repeat} from "../kofi-utils/src/repeat.js";
+export {default as sign} from "../kofi-utils/src/sign.js";
+export {default as snakeCase} from "../kofi-utils/src/snake-case.js";
+export {default as timer} from "../kofi-utils/src/timer.js";
+export {default as timestamp} from "../kofi-utils/src/timestamp.js";
+export {default as truncate} from "../kofi-utils/src/truncate.js";
+export {default as uniqueId} from "../kofi-utils/src/unique-id.js";
+export {default as values} from "../kofi-utils/src/values.js";
