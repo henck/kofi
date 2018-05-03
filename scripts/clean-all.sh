90016898785241aef1046d9c09330f74fe742f22
jmjuanes <josemijuanes@gmail.com>
2018-05-03 20:02:06 +0200

    Minor changes

--- scripts/clean-all.sh
+++ scripts/clean-all.sh
@@ -3,8 +3,9 @@ set -e
 
 # Clean all packages in the file releasable-packages.txt
 for PKG in $(cat ./releasable-packages.txt) ; do
-    echo "Cleaning package ${PKG}..."
-    rm -rf ./packages/${PKG}/.dist 
-    rm -rf ./packages/${PKG}/.bundle
+    #echo "Cleaning package ${PKG}..."
+    #rm -rf ./packages/${PKG}/.dist 
+    #rm -rf ./packages/${PKG}/.bundle
+    make clean ${PKG}
 done
 
