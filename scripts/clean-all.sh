03f87b95155b96a59a9bd29be38989607db0859c
jmjuanes <josemijuanes@gmail.com>
2018-05-03 16:05:21 +0200

    Print the package name

--- scripts/clean-all.sh
+++ scripts/clean-all.sh
@@ -3,6 +3,7 @@ set -e
 
 # Clean all packages in the file releasable-packages.txt
 for PKG in $(cat ./releasable-packages.txt) ; do
+    echo "Cleaning package ${PKG}..."
     rm -rf ./packages/${PKG}/.dist 
     rm -rf ./packages/${PKG}/.bundle
 done
