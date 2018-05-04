8d1a43b411158aac97b9a1dcfe098621a3fb3dfe
jmjuanes <josemijuanes@gmail.com>
2018-05-04 13:21:43 +0200

    Init dist placeholder

---
+++ generators/npm.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === "production") {
+          module.exports = require("./kofi-{{package}}.min.js");
+} else {
+          module.exports = require("./kofi-{{package}}.js");
+}
+
