9acf3a328a9f3e7ad1a92ba57ebfed8e1e1cd182
jmjuanes <josemijuanes@gmail.com>
2018-06-05 19:37:00 +0200

    Init ready method

---
+++ packages/dom/src/ready.js
@@ -0,0 +1,12 @@
+export default function ready (fn) {
+    if (document.readyState !== 'loading') {
+        return fn(null);
+    }
+    document.addEventListener('DOMContentLoaded', function (event) {
+        if (document.readyState === 'loading') {
+            return;
+        }
+        return fn(event);
+    });
+};
+
