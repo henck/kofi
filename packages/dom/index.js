f663c6d1039b16acc3766ead75217f92777d84cf
jmjuanes <josemijuanes@gmail.com>
2018-12-22 11:52:38 +0100

    Updated exports

--- packages/dom/index.js
+++ packages/dom/index.js
@@ -1,13 +1,8 @@
-import {createElement, mountElement, updateElement} from "./src/element.js";
-import {createComponent, mountComponent} from "./src/component.js";
+import dom from "./src/dom.js";
 import ready from "./src/ready.js";
 
 export {
-    createElement,
-    mountElement,
-    updateElement,
-    createComponent,
-    mountComponent,
+    dom,
     ready
 };
 
