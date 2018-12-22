d5b652ba4d7cfe58b0c3d7e66145ad8cf4936af4
jmjuanes <josemijuanes@gmail.com>
2018-12-22 11:56:48 +0100

    Init app entry point

---
+++ packages/app/index.js
@@ -0,0 +1,10 @@
+import {createElement, mountElement, updateElement} from "./src/element.js";
+import {createComponent, mountComponent} from "./src/component.js";
+
+export {
+    createElement,
+    mountElement,
+    updateElement,
+    createComponent,
+    mountComponent
+};
