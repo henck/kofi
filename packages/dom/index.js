09fa7ff4fd637f26d2a62bc95976ae8f7819b372
jmjuanes <josemijuanes@gmail.com>
2019-05-01 11:45:57 +0200

    Export all nodes management

--- packages/dom/index.js
+++ packages/dom/index.js
@@ -1,8 +1,6 @@
-import dom from "./src/dom.js";
-import ready from "./src/ready.js";
-
-export {
-    dom,
-    ready
-};
+export {createNode, removeNode, emptyNode} from "./src/node.js";
+export {setNodeAttributes, setNodeChildren} from "./src/node.js";
+export {ready} from "./src/ready.js";
+export {createRef} from "./src/ref.js";
+export {Fragment} from "./src/fragment.js";
 
