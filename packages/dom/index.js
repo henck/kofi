8b536c963875cd692818694c0c7efed948310f5b
jmjuanes <josemijuanes@gmail.com>
2019-05-01 20:04:49 +0200

    Updated exports

--- packages/dom/index.js
+++ packages/dom/index.js
@@ -1,5 +1,5 @@
-export {createNode, removeNode, emptyNode} from "./src/node.js";
-export {setNodeAttributes, setNodeChildren} from "./src/node.js";
+export {createNode, removeNode} from "./src/node.js";
+export {addNodeChildren, removeNodeChildren} from "./src/children.js";
 export {ready} from "./src/ready.js";
 export {createRef} from "./src/ref.js";
 export {Fragment} from "./src/fragment.js";
