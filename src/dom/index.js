5b8563b2ddf228419e5c07e7ae4c8ada110dd8db
Josemi <josemijuanes@gmail.com>
2019-07-03 11:19:50 +0200

    Init dom modules export

---
+++ src/dom/index.js
@@ -0,0 +1,5 @@
+export {createNode, removeNode} from "./node.js";
+export {addNodeChildren, removeNodeChildren} from "./children.js";
+export {createRef} from "./ref.js";
+export {Fragment} from "./fragment.js";
+
