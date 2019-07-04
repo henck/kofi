c39bd968c40a40e46729b8dd06e86f315fe56371
Josemi <josemijuanes@gmail.com>
2019-07-04 11:56:18 +0200

    Moved to src folder. Fixed exports

---
+++ src/index.js
@@ -0,0 +1,9 @@
+export * from "./dispatch.js";
+export * from "./md.js";
+export * from "./queue.js";
+export * from "./ready.js";
+export * from "./dom/index.js";
+export * from "./helpers/index.js";
+export * from "./request/request.js";
+export * from "./router/index.js";
+
